dfa3e5d456d45aa7b7dbf354aa3b04842e9d5c59
prbs5kong <prbs5kong@gmail.com>
2023-11-20 23:03:01 -0500

    new attention loss

--- configs/data_configs.py
+++ configs/data_configs.py
@@ -5,8 +5,8 @@ from configs.paths_config import dataset_paths
 DATASETS = {
 	'ffhq_encode': {
 		'transforms': transforms_config.EncodeTransforms,
-		'train_source_root': dataset_paths['ffhq'],
-		'train_target_root': dataset_paths['ffhq'],
+		'train_source_root': dataset_paths['celeba_train'],
+		'train_target_root': dataset_paths['celeba_train'],
 		'test_source_root': dataset_paths['celeba_test'],
 		'test_target_root': dataset_paths['celeba_test'],
 	},
